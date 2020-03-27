389721f49dfb7b6121a4574732f934f40ad4f36a
sangwook0613 <kvmoke@gmail.com>
2020-03-27 16:29:22 +0900

    #3.4 Admin Panel

--- users/admin.py
+++ users/admin.py
@@ -2,6 +2,10 @@ from django.contrib import admin
 from . import models
 
 # Register your models here.
-@admin.register(models.User)
+@admin.register(models.User)  # decorator는 바로 위에 있어야함(공백없이)
 class CustomUserAdmin(admin.ModelAdmin):
-    pass
+
+    """ Custom User Admin """
+
+    list_display = ("username", "email", "gender", "language", "currency", "superhost")
+    list_filter = ("language", "currency", "superhost")
