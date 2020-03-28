aee8baca8bf5d7740924e4aa2b4ed58ebe6f36e0
sangwook0613 <kvmoke@gmail.com>
2020-03-28 12:03:54 +0900

    #4.1 Room Model part One

--- rooms/models.py
+++ rooms/models.py
@@ -1,9 +1,24 @@
-from django.db import models
-from core import models as core_models
+from django.db import models  # 먼저 장고를 받고
+from django_countries.fields import CountryField  # 그 다음 외부패키지
+from core import models as core_models  # 마지막으로 내가 만든 패키지
+from users import models as user_models
 
 # Create your models here.
 class Room(core_models.TimeStampedModel):
 
     """ Room Model Defintion """
 
-    pass
+    name = models.CharField(max_length=140)
+    description = models.TextField()
+    country = CountryField()
+    city = models.CharField(max_length=80)
+    price = models.IntegerField()
+    address = models.CharField(max_length=140)
+    beds = models.IntegerField()
+    bedrooms = models.IntegerField()
+    baths = models.IntegerField()
+    guests = models.IntegerField()
+    check_in = models.TimeField()
+    check_out = models.TimeField()
+    instant_book = models.BooleanField(default=False)
+    host = models.ForeignKey(user_models.User, on_delete=models.CASCADE)
