91ba6c56edeb4b9ea9f337d2a47a24471e9830ae
sangwook0613 <kvmoke@gmail.com>
2020-04-01 01:26:08 +0900

    #7.3 Finishing the Room Admin

--- rooms/admin.py
+++ rooms/admin.py
@@ -7,6 +7,11 @@ class ItemAdmin(admin.ModelAdmin):
 
     """ Item Admin Definition """
 
+    list_display = ("name", "used_by")
+
+    def used_by(self, obj):
+        return obj.rooms.count()
+
     pass
 
 
