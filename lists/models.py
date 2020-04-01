699013d221d8fd045240f2e6caf720ad9a7a48b7
sangwook0613 <kvmoke@gmail.com>
2020-04-01 13:21:30 +0900

    #8.2 Conversations, Lists, Reservations Admin

--- lists/models.py
+++ lists/models.py
@@ -8,9 +8,14 @@ class List(core_models.TimeStampedModel):
 
     name = models.CharField(max_length=80)
     user = models.ForeignKey(
-        "users.User", related_name="lists", on_delete=models.CASCADE
+        "users.User", related_name="list", on_delete=models.CASCADE
     )
-    room = models.ManyToManyField("rooms.Room", related_name="lists", blank=True)
+    rooms = models.ManyToManyField("rooms.Room", related_name="lists", blank=True)
 
     def __str__(self):
         return self.name
+
+    def count_rooms(self):
+        return self.rooms.count()
+
+    count_rooms.short_description = "Number of Rooms"
