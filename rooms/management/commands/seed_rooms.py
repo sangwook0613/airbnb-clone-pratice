59340e435cae153bed7e01ff279b71f5ea945d1f
sangwook0613 <kvmoke@gmail.com>
2020-04-03 21:00:27 +0900

    #9.3 seed_rooms Part 1

---
+++ rooms/management/commands/seed_rooms.py
@@ -0,0 +1,37 @@
+import random
+from django.core.management.base import BaseCommand
+from django_seed import Seed
+from rooms import models as room_models
+from users import models as user_models
+
+
+class Command(BaseCommand):
+
+    help = "This command creates many users"
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            "--number", default=2, type=int, help="How many users do you want to create"
+        )
+
+    def handle(self, *args, **options):
+        number = options.get("number")
+        seeder = Seed.seeder()
+        all_users = user_models.User.objects.all()  # 데이터가 많다면 당연히 all을 쓰면 안된다.
+        room_types = room_models.RoomType.objects.all()
+        seeder.add_entity(
+            room_models.Room,
+            number,
+            {
+                "name": lambda x: seeder.faker.address(),
+                "host": lambda x: random.choice(all_users),
+                "room_type": lambda x: random.choice(room_types),
+                "guests": lambda x: random.randint(0, 10),
+                "price": lambda x: random.randint(0, 300),
+                "beds": lambda x: random.randint(0, 5),
+                "bedrooms": lambda x: random.randint(0, 4),
+                "baths": lambda x: random.randint(0, 4),
+            },
+        )
+        seeder.execute()
+        self.stdout.write(self.style.SUCCESS(f"{number} rooms created!"))
