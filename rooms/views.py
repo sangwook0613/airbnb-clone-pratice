83f97fa73dd7fbfad822ea32f64ae829eee44ef2
sangwook0613 <kvmoke@gmail.com>
2020-04-30 00:13:02 +0900

    #11.1 Pagination with Limit & Offset

--- rooms/views.py
+++ rooms/views.py
@@ -3,7 +3,11 @@ from . import models
 
 
 def all_rooms(request):  # core urls.py에서의 이름과 같아야되며
-    all_rooms = models.Room.objects.all()
+    page = int(request.GET.get("page", 1))
+    page_size = 10
+    limit = page_size * page
+    offset = limit - page_size
+    all_rooms = models.Room.objects.all()[offset:limit]
     return render(
         request, "rooms/home.html", context={"rooms": all_rooms}
     )  # templates 폴더에 있는 html파일 이름과 같아야하고 context의 제목은 html에 똑같이 쓰여야한다.
