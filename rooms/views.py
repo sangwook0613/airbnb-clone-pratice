93a45708813b8a989221ab816831f38b3ebdcb57
sangwook0613 <kvmoke@gmail.com>
2020-05-11 20:30:23 +0900

    #11.6 Handling Expections

--- rooms/views.py
+++ rooms/views.py
@@ -1,15 +1,16 @@
-from math import ceil
-from django.shortcuts import render
-from django.core.paginator import Paginator
+from django.shortcuts import render, redirect
+from django.core.paginator import Paginator, EmptyPage
 from . import models
 
 
 def all_rooms(request):
     page = request.GET.get("page", 1)
     room_list = models.Room.objects.all()
-    paginator = Paginator(
-        room_list, 10, orphans=5
-    )  # orphans는 그 수 이하의 아이템들이 남았을때, 새로운 페이지를 만들지 않고 가장 마지막 페이지에 합쳐줌, 여기서 만약 마지막 페이지가 6개가 남았다면 새로운 페이지로 유지된다.
-    rooms = paginator.page(int(page))
-    print(vars(rooms.paginator))
-    return render(request, "rooms/home.html", {"page": rooms})
+    paginator = Paginator(room_list, 10, orphans=5)
+
+    try:
+        rooms = paginator.page(int(page))
+        return render(request, "rooms/home.html", {"page": rooms})
+    except EmptyPage:  # EmptyPage 대신에 Exception을 두면 모든 에러에 대해서 예외처리를 함
+        # 하지만, 각각의 에러에 맞게 대처하는 상황을 제시하는 것이 낫다.
+        return redirect("/")
