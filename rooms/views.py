bad2f56356201664c6eac4ca9020454d4cf001e0
sangwook0613 <kvmoke@gmail.com>
2020-05-11 20:21:47 +0900

    #11.5 get_page vs page

--- rooms/views.py
+++ rooms/views.py
@@ -5,9 +5,11 @@ from . import models
 
 
 def all_rooms(request):
-    page = int(request.GET.get("page", 1))
+    page = request.GET.get("page", 1)
     room_list = models.Room.objects.all()
-    paginator = Paginator(room_list, 10)
-    rooms = paginator.get_page(page)
+    paginator = Paginator(
+        room_list, 10, orphans=5
+    )  # orphans는 그 수 이하의 아이템들이 남았을때, 새로운 페이지를 만들지 않고 가장 마지막 페이지에 합쳐줌, 여기서 만약 마지막 페이지가 6개가 남았다면 새로운 페이지로 유지된다.
+    rooms = paginator.page(int(page))
     print(vars(rooms.paginator))
-    return render(request, "rooms/home.html", {"rooms": rooms})
+    return render(request, "rooms/home.html", {"page": rooms})
