7496ff1684f8f3555154019747b5565f8d7d1dad
sangwook0613 <kvmoke@gmail.com>
2020-05-12 13:30:25 +0900

    #12.0 URLs and Arguments

--- rooms/views.py
+++ rooms/views.py
@@ -1,5 +1,6 @@
 from django.utils import timezone
 from django.views.generic import ListView
+from django.shortcuts import render
 from . import models
 
 
@@ -13,8 +14,7 @@ class HomeView(ListView):
     ordering = "created"
     context_object_name = "rooms"
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)  # super를 안부르면 컨텐츠들을 못 부른다!
-        now = timezone.now()
-        context["now"] = now
-        return context
+
+def room_detail(request, pk):
+    
+    return render(request, "rooms/detail.html")
