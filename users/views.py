3b40437e0bd7685e0138291093abddbc3b7d30df
sangwook0613 <kvmoke@gmail.com>
2020-05-15 01:11:38 +0900

    #15.1 Signup Form part 2

--- users/views.py
+++ users/views.py
@@ -31,3 +31,13 @@ class SignUpView(FormView):
     form_class = forms.SignUpForm
     success_url = reverse_lazy("core:home")
     initial = {"first_name": "Simon", "last_name": "Park", "email": "hi@hello.com"}
+
+    def form_valid(self, form):
+        form.save()
+        email = form.cleaned_data.get("email")
+        password = form.cleaned_data.get("password")
+        user = authenticate(self.request, username=email, password=password)
+        if user is not None:
+            login(self.request, user)
+
+        return super().form_valid(form)
