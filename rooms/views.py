697d833269dc6880f3f4aa64f826637867f9f371
sangwook0613 <kvmoke@gmail.com>
2020-05-25 20:32:52 +0900

    #23.6 Add Photo part 2

--- rooms/views.py
+++ rooms/views.py
@@ -180,13 +180,15 @@ class EditPhotoView(user_mixins.LoggedInOnlyView, SuccessMessageMixin, UpdateVie
         return reverse("rooms:photos", kwargs={"pk": room_pk})
 
 
-class AddPhotoView(user_mixins.LoggedInOnlyView, SuccessMessageMixin, FormView):
+class AddPhotoView(user_mixins.LoggedInOnlyView, FormView):
 
     model = models.Photo
     template_name = "rooms/photo_create.html"
     fields = ("caption", "file")
     form_class = forms.CreatePhotoForm
 
-    def form_vaild(self, form):
+    def form_valid(self, form):
         pk = self.kwargs.get("pk")
         form.save(pk)
+        messages.success(self.request, "Photo Uploaded")
+        return redirect(reverse("rooms:photos", kwargs={"pk": pk}))
