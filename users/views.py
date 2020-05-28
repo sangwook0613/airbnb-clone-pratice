f85518cabbece7e6ae29b80c4b4b850ab6728741
sangwook0613 <kvmoke@gmail.com>
2020-05-28 13:40:05 +0900

    #25.1 Language Select

--- users/views.py
+++ users/views.py
@@ -1,5 +1,6 @@
 import os
 import requests
+from django.http import HttpResponse
 from django.contrib.auth.views import PasswordChangeView
 from django.views.generic import FormView, DetailView, UpdateView
 from django.urls import reverse_lazy
@@ -268,6 +269,7 @@ class UpdatePasswordView(
     def get_success_url(self):
         return self.request.user.get_absolute_url()
 
+
 @login_required
 def switch_hosting(request):
     try:
@@ -275,3 +277,10 @@ def switch_hosting(request):
     except KeyError:
         request.session["is_hosting"] = True
     return redirect(reverse("core:home"))
+
+
+def switch_language(request):
+    lang = request.GET.get("lang", None)
+    if lang is not None:
+        pass
+    return HttpResponse(status=200)
